fb092378cbddba41c36a8ca2a5b0317ed84da45d
emilyb102 <44143101+emilyb102@users.noreply.github.com>
2020-10-15 23:07:32 -0500

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,40 +1,108 @@
-const gentleText = document.getElementById("gentleText");
-const goofyText = document.getElementById("goofyText");
-const inspiredText = document.getElementById("inspiredText");
-
-const gentleBtn = document.getElementById("gentleBtn");
-const goofyBtn = document.getElementById("goofyBtn");
-const inspiredBtn = document.getElementById("inspiredBtn");
-const headerBtn = document.querySelector("p");
-
-gentleText.textContent = newQuestion(Gentle);
-goofyText.textContent = newQuestion(Goofy);
-inspiredText.textContent = newQuestion(Inspired);
+//initialize: declare the card array and push the proper number of cards 
+generateCards();
 
 
-gentleBtn.addEventListener("click", function(){
-	gentleText.textContent = newQuestion(Gentle);
-});
 
-goofyBtn.addEventListener("click", function(){
-	goofyText.textContent = newQuestion(Goofy);
-});
 
-inspiredBtn.addEventListener("click", function(){
-	inspiredText.textContent = newQuestion(Inspired);
-});
+const hardCard = document.querySelectorAll(".hardcard");
+const quoteContainer = document.querySelectorAll(".quoteContainer");
+const text = document.querySelectorAll(".text");
+const quoteBox = document.querySelectorAll(".quoteBox");
+const quoteText = document.querySelector(".quoteContainer");
 
-headerBtn.addEventListener("click", function(){
-	gentleText.textContent = newQuestion(Gentle);
-	goofyText.textContent = newQuestion(Goofy);
-	inspiredText.textContent = newQuestion(Inspired);
+
+
+
+
+
+const header = document.querySelector(".header");
+const buttons = document.querySelectorAll("button");
+for(var i = 0; i<hardCard.length; i++){
+	buttons[i].textContent = allQuestions[i].name
+	quoteContainer[i].style.backgroundImage = "url(Assets/images/" + allQuestions[i].image + ")";
+}
+hoverHeader();
+hoverButton();
+btnClick();
+allClick();
+all();
+
+
+
+
+
+quoteContainer.forEach(function(element, index){
+	if (element.textContent.length >= 175){
+		quoteBox[(index)].classList.add("overflow")
+	} else {
+		quoteBox[(index)].classList.remove("overflow")
+	}
 })
 
 
-
-
-
-function newQuestion (type){
-	const quote = type[Math.floor(Math.random()*type.length)];
-	return quote.text; 
+function generateCards(){
+	const cardNum = 3;
+const cardNumArr = [];
+const fixed = document.querySelector(".fixed");
+const removable = document.querySelector(".removable");
+const card = fixed.innerHTML
+for(var i = 0; i<cardNum; i++){
+	cardNumArr.push(card);
 }
+removable.innerHTML=cardNumArr.concat()
+}
+
+function newQuestion(type){
+	for(var i = 0; i<allQuestions.length; i++){
+		if (allQuestions[i].name === type) {
+			const quote = allQuestions[i].questions[Math.floor(Math.random()*allQuestions[i].questions.length)]; 
+			return quote.text;
+		}
+	}
+
+}
+
+function btnClick(){
+	buttons.forEach(function(element, index){
+		element.addEventListener("click", function(){
+			text[(index)].textContent = newQuestion(element.textContent)
+		})
+	});
+}
+
+
+
+function all (){
+	text.forEach(function(element, index){
+		element.textContent = newQuestion(buttons[(index)].textContent)
+	})
+}
+function allClick (){
+	header.addEventListener("click", function(){
+	text.forEach(function(element, index){
+		element.textContent = newQuestion(buttons[(index)].textContent)
+	})
+})
+}
+
+header.addEventListener("click", all);
+document.addEventListener("keydown", all);
+
+function hoverHeader (){
+header.addEventListener("mouseover", function(){
+	this.classList.add("headerHover");
+})
+header.addEventListener("mouseout", function(){
+	this.classList.remove("headerHover");
+})
+}
+
+function hoverButton (){
+for(var i = 0; i<buttons.length; i++){
+	buttons[i].addEventListener("mouseover", function (){
+		this.classList.add("buttonHover");
+	})
+	buttons[i].addEventListener("mouseout", function (){
+		this.classList.remove("buttonHover");
+	})
+}}
