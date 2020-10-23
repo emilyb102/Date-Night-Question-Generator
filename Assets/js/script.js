0958b26faf9b883522bd3540fb86c331ef440d4e
emilyb102 <44143101+emilyb102@users.noreply.github.com>
2020-10-23 11:05:22 -0500

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,162 +1,159 @@
+//Sound Effects
 const button = new Audio("Assets/sounds/buttontap.wav");
 const pen = new Audio("Assets/sounds/penclick.mp3");
-// const bottle = new Audio("Assets/sounds/bottlecrush.mp3");
-
 const bottle = new Audio("Assets/sounds/bottlecrush.wav");
-const buttonHover = "buttonHover";
-const checkHover = "checkHover";
-//generate checkboxes & push in names of all question lists
+
+//Initialization Constants
+const fixedCheck = document.querySelector("div.checkboxribbon .fixed");
+const variableCheck = document.querySelector("div.checkboxribbon .variable");
+const checkNum = allQuestions.length - 1;
 const checkArray = [];
-const fixedCheck = document.querySelector(".fixedCheck");
-const removeCheck = document.querySelector(".removeCheck");
-const check = fixedCheck.innerHTML
-for(var i=1; i<allQuestions.length; i++){
-	checkArray.push(check);
-}
-removeCheck.innerHTML = checkArray.concat()
+const fixed = document.querySelector("div.hardcardRibbon .fixed");
+const variable = document.querySelector("div.hardcardRibbon .variable");
+const cardNum = allQuestions.length - 1;
+const cardNumArr = [];
+
+//Initialize: generate & push in # of checkboxes & hardcards, based on index of allQuestions
+generateFromFixedSkeleton(allQuestions, checkArray, fixedCheck, variableCheck);
+generateFromFixedSkeleton(allQuestions, cardNumArr, fixed, variable);
+
+//Post initialization constants
 const checkboxLabel = document.querySelectorAll(".checkboxLabel");
-for(var i = 0; i<allQuestions.length; i++){
+const hardCard = document.querySelectorAll(".hardcard");
+const buttons = document.querySelectorAll("button");
+const quoteContainer = document.querySelectorAll(".quoteContainer");
+const chkBoxSquare = document.querySelectorAll(".chkBoxSquare")
+const checkboxContent = document.querySelectorAll(".checkboxContent");
+const text = document.querySelectorAll(".text");
+const header = document.querySelector(".header");
+const quoteBox = document.querySelectorAll(".quoteBox");
+
+//Label: put in checkbox labels, and hard card label and picture
+//.....Checkboxes......
+for (var i = 0; i < allQuestions.length; i++) {
 	checkboxLabel[i].textContent = allQuestions[i].name
 }
-const checkboxContent = document.querySelectorAll(".checkboxContent");
-const chkBoxSquare = document.querySelectorAll(".chkBoxSquare")
-//assign default of first 3 as checked
-for(var i=1; i<4; i++){
-	chkBoxSquare[i].classList.add("checkedBox");
-}
-
-
-
-//initialize: declare the card array and push the proper number (really every single) of cards 
-const cardNum = allQuestions.length-1;
-const cardNumArr = [];
-const fixed = document.querySelector(".fixed");
-const removable = document.querySelector(".removable");
-generateCards();
-
-
-function generateCards(type){
-
-const card = fixed.innerHTML
-for(var i = 0; i<cardNum; i++){
-	cardNumArr.push(card);
-}
-removable.innerHTML=cardNumArr.concat()
-}
-
-
-
-
-
-
-const hardCard = document.querySelectorAll(".hardcard");
-const quoteContainer = document.querySelectorAll(".quoteContainer");
-const text = document.querySelectorAll(".text");
-const quoteBox = document.querySelectorAll(".quoteBox");
-const quoteText = document.querySelector(".quoteContainer");
-
-
-
-
-
-
-const header = document.querySelector(".header");
-const buttons = document.querySelectorAll("button");
-for(var i = 0; i<hardCard.length; i++){
+//.....Hardcards.......
+for (var i = 0; i < hardCard.length; i++) {
 	buttons[i].textContent = allQuestions[i].name
 	quoteContainer[i].style.backgroundImage = "url(Assets/images/" + allQuestions[i].image + ")";
 }
 
-//now make the cards that are not the first 3 display: none
-for(var i=4; i<allQuestions.length; i++){
+//Control: make first 3 checkboxes always checked, and make cards that aren't first 3 display 'none'
+//.....Checkbox checks.......
+for (var i = 1; i < 4; i++) {
+	chkBoxSquare[i].classList.add("checkedBox");
+}
+//.......Hardcard display.........
+for (var i = 4; i < allQuestions.length; i++) {
 	hardCard[i].classList.add("uncheckedBox");
 }
 
-hoverHeader();
-hoverButton(buttons, buttonHover);
-hoverButton(chkBoxSquare, checkHover);
-btnClick();
-allClick();
-all();
-
-
-
-
-
-quoteContainer.forEach(function(element, index){
-	if (element.textContent.length >= 175){
-		quoteBox[(index)].classList.add("overflow")
-	} else {
-		quoteBox[(index)].classList.remove("overflow")
-	}
-})
-
-//If box is checked/unchecked, apply display none to hardcard
-checkboxContent.forEach(function(element, index){
-	element.addEventListener("click", function (){
+//Checkbox logic: connect checkboxes & hardcards
+//If box is checked/unchecked, apply display none to hardcard + Styling
+checkboxContent.forEach(function (element, index) {
+	element.addEventListener("click", function () {
 		pen.play();
 		chkBoxSquare[(index)].classList.toggle("checkedBox");
 		hardCard[(index)].classList.toggle("uncheckedBox");
 	})
 })
 
+//Questions: Generate new question for each box
+all();
 
-function newQuestion(type){
-	for(var i = 0; i<allQuestions.length; i++){
-		if (allQuestions[i].name === type) {
-			const quote = allQuestions[i].questions[Math.floor(Math.random()*allQuestions[i].questions.length)]; 
-			return quote.text;
-		}
+
+//Question logic, when a button or header is clicked (or any new key pressed), generate new question/s
+allClick();
+btnClick();
+header.addEventListener("click", all);
+document.addEventListener("keydown", all);
+
+//Styling: hover, text control
+//.....Hover.....
+hoverStyle(header, "headerHover");
+hoverStyle(buttons, "buttonHover");
+hoverStyle(chkBoxSquare, "checkHover");
+//.....Text control.....
+quoteContainer.forEach(function (element, index) {
+	if (element.textContent.length >= 175) {
+		quoteBox[(index)].classList.add("overflow");
+	} else {
+		quoteBox[(index)].classList.remove("overflow");
 	}
+})
 
+
+
+
+//Functions
+function generateFromFixedSkeleton(controlArray, emptyArray, fixed, variable) {
+	for (var i = 0; i < controlArray.length - 1; i++) {
+		emptyArray.push(fixed.innerHTML);
+	}
+	variable.innerHTML = emptyArray.join('');
 }
-
-function btnClick(){
-	buttons.forEach(function(element, index){
-		element.addEventListener("click", function(){
+function all() {
+	text.forEach(function (element, index) {
+		element.textContent = newQuestion(buttons[(index)].textContent)
+	})
+}
+function allClick() {
+	header.addEventListener("click", function () {
+		text.forEach(function (element, index) {
+			element.textContent = newQuestion(buttons[(index)].textContent)
+			bottle.play();
+		})
+	})
+}
+function btnClick() {
+	buttons.forEach(function (element, index) {
+		element.addEventListener("click", function () {
 			button.play();
 			text[(index)].textContent = newQuestion(element.textContent)
 		})
 	});
 }
-
-
-
-function all (){
-	text.forEach(function(element, index){
-		element.textContent = newQuestion(buttons[(index)].textContent)
-	})
+function newQuestion(type) {
+	for (var i = 0; i < allQuestions.length; i++) {
+		if (allQuestions[i].name === type) {
+			const quote = allQuestions[i].questions[Math.floor(Math.random() * allQuestions[i].questions.length)];
+			return quote.text;
+		}
+	}
 }
-function allClick (){
-	header.addEventListener("click", function(){
-	text.forEach(function(element, index){
-		element.textContent = newQuestion(buttons[(index)].textContent)
-		bottle.play();
-	})
-})
+function hoverStyle(element, style) {
+	if (element.length === undefined) {
+		element.addEventListener("mouseover", function () {
+			this.classList.add(style);
+		})
+		element.addEventListener("mouseout", function () {
+			this.classList.remove(style);
+		})
+	} else {
+		element.forEach(function (element) {
+			element.addEventListener("mouseover", function () {
+				this.classList.add(style);
+			})
+			element.addEventListener("mouseout", function () {
+				this.classList.remove(style);
+			})
+		})
+	}
 }
 
-header.addEventListener("click", all);
-document.addEventListener("keydown", all);
 
-function hoverHeader (){
-header.addEventListener("mouseover", function(){
-	this.classList.add("headerHover");
-})
-header.addEventListener("mouseout", function(){
-	this.classList.remove("headerHover");
-})
-}
 
-function hoverButton (type, style){
-for(var i = 0; i<type.length; i++){
-	type[i].addEventListener("mouseover", function (){
-		this.classList.add(style);
 
-	})
-	type[i].addEventListener("mouseout", function (){
-		this.classList.remove(style);
-		
-	})
-}}
+
+
+
+
+
+
+
+
+
+
+
 
